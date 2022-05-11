57a11ec4d9670c203136044fceba925f9ea67098
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2022-05-11 23:33:25 +0300

    Create 5. Longest Palindromic Substring.py

---
+++ 5. Longest Palindromic Substring.py
@@ -0,0 +1,21 @@
+s = "babad"
+def longestPalindrome(s):
+    def IsPalindrome(s):
+        if s == s[::-1]:
+            return True
+        else:
+            return False
+    if s == "":
+        return s
+    ansLen = 1
+    ansStart = 0
+    for i in range(1,len(s)):
+        checkRange = i - ansLen
+        if checkRange > 0 and IsPalindrome(s[checkRange-1:i+1]):
+            ansLen += 2
+            ansStart = checkRange-1
+        elif checkRange >= 0 and  IsPalindrome(s[checkRange:i+1]):
+            ansLen += 1
+            ansStart = checkRange
+    return s[ansStart:ansStart + ansLen]
+print(longestPalindrome(s))
