50480cf69fd22ad41d05da343a4fa08638177241
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2022-07-06 13:12:02 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def fizzBuzz(self, n):
         """
         :type n: int
@@ -22,22 +21,3 @@ def fizzBuzz(self, n):
                 ans.append(str(i))
 
         return ans
-=======
-class Solution:
-    def fizzBuzz(self, n) -> List[str]:
-        
-        input = []
-        
-        for i in range(1,n+1):  
-        
-            if i % 3 == 0 and i % 5 == 0:
-                input.append("FizzBuzz")           
-            elif i % 3 == 0:                
-                input.append("Fizz")            
-            elif i % 5 == 0:
-                input.append("Buzz")
-            else:
-                input.append(str(i))
-                
-        return input
->>>>>>> a5ee5542be08b3b99e1456b3d44544b177d9a53f
