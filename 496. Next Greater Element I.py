7b3ae920a2a0d1de53bb8e35411e89c71fc65f2d
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2022-08-25 17:02:24 +0300

    Update 496. Next Greater Element I.py

--- 496. Next Greater Element I.py
+++ 496. Next Greater Element I.py
@@ -12,3 +12,19 @@ class Solution:
                         array[nums1.index(nums2[i])]=nextgreater
                         break
         return array
+    
+    
+    class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack=[]
+        res=[-1 for i in range(len(nums1))]
+        for i in range(len(nums2)):
+            cur=nums2[i]
+
+            while stack and cur>stack[-1]:
+                val=stack.pop()
+                res[nums1.index(val)]=cur
+            if cur in nums1:
+                stack.append(cur)
+        return res
+
