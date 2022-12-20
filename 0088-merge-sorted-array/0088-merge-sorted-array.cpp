0bb44cdab094a478537fe8822ae87a900748de9d
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2022-12-20 19:40:11 +0300

    Time: 76 ms (36.69%), Space: 14 MB (6.38%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,10 +1,11 @@
-class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
-        }
-        sort(nums1.begin(),nums1.end());
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        j=0
+        for i in range(len(nums1)-1,len(nums1)-n-1,-1):
+            nums1[i]=nums2[j]
+            j+=1
+        return nums1.sort()
         
-    }
-};
