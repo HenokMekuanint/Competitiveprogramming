9201dfeddc4b2497470882a19184068b68b14d02
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2022-12-22 17:59:11 +0300

    Create pillingup.py

---
+++ pillingup.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+for _ in range(int(input())):
+    _, queue = input(), deque(map(int, input().split()))
+    for cube in reversed(sorted(queue)):
+        if queue[-1] == cube: queue.pop()
+        elif queue[0] == cube: queue.popleft()
+        else:
+             print('No')
+             break
+    else:
+         print('Yes')
