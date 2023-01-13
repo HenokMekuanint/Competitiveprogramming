88c85784c3c796e61e96e7b0d77fd70aab352771
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-01-13 20:02:11 +0300

    Time: 481 ms (18.01%), Space: 13.9 MB (77.12%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,22 +1,12 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # true if i-th friend is left
-        friends = [False] * n
-
-        friendCount = n
-        fp = 0  # friends' pointer
-
-        while friendCount > 1:
-          for _ in range(k):
-            while friends[fp % n]:  # the friend is not there
-              fp += 1  # point to the next one
-            fp += 1
-          friends[(fp - 1) % n] = True
-          friendCount -= 1
-
-        fp = 0
-        while friends[fp]:
-          fp += 1
-
-        return fp + 1
+        ans=[i for i in range(n,0,-1)]
+        n=len(ans)
+        while n>1:
+            for i in range(k-1):
+                popped=ans.pop()
+                ans.insert(0,popped)
+            ans.pop()
+            n-=1
+        return ans[0]
         
