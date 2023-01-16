61d4edf8726ed424ee7559c6f27895135a527a54
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-01-16 15:34:27 +0300

    Time: 50 ms (84.97%), Space: 13.9 MB (60.18%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,16 +1,5 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        """
-        words=["bella","label","roller"]
-        word="bella"
-        i=0
-        dict={
-        b:[[0,1]],
-        e:[[0,1]],
-        l:[[0,2],[1,l]]
-        a:[[0,1]]
-        }
-        """
         ans=[]
         for i in range(97,124):
             char=chr(i)
@@ -22,7 +11,6 @@ class Solution:
             ans.extend([char]*min(temp))
         return ans
             
-                
                 
             
             
