a308f3fb4f3f0e9d897387fdc4529a6683caacf0
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-02-24 15:41:18 +0300

    Time: 206 ms (29.11%), Space: 13.8 MB (99.75%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,27 +1,29 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # s = "AABABBA", k = 2
-        #      L
-        #         R
-        #     windowlen=r-l+1
-        #         {
-        #             A:3
-        #             B:1
-        #         }
-        #         3-2<=2
+        """
+        "   A   B   A   B   "
         
-            left=0
-            dicti=defaultdict(int)
-            ans=0
-            for right in range(len(s)):
-                dicti[s[right]]+=1
-                while (right-left+1)-max(dicti.values())>k:
-                    dicti[s[left]]-=1
-                    left+=1
-                ans=max(ans,right-left+1)
-            return ans
-                
-
-
+            L
             
-  
+                R
+            dicti={
+            "A":1,
+            "B":1,
+            
+            
+            }
+        
+        """
+        dicti=defaultdict(int)
+        left=0
+        ans=float("-inf")
+        for right in range(len(s)):
+            dicti[s[right]]+=1
+            while(sum(dicti.values())-max(dicti.values()))>k:
+                dicti[s[left]]-=1
+                left+=1
+            ans=max(ans,right-left+1)
+        return ans
+            
+            
+            
