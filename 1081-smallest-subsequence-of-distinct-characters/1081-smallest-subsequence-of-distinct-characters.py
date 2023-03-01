d538ed58ac291b8b6e5c802f61ea668c03945bd7
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-03-01 15:31:54 +0300

    Time: 42 ms (26.24%), Space: 13.8 MB (97.03%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,25 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        
+        stack=[]
+        freq=defaultdict(int)
+        visited=defaultdict(bool)
+        for i in range(len(s)):
+            freq[s[i]]+=1
+            if s[i] not in visited:
+                visited[s[i]]=False
+        
+        for i in range(len(s)):
+            
+            if visited[s[i]]:
+                freq[s[i]]-=1
+                continue
+            else:
+                while stack and stack[-1]>ord(s[i]) and freq[chr(stack[-1])]>0:
+                   
+                    visited[chr(stack[-1])]=False
+                    stack.pop()
+                stack.append(ord(s[i]))
+                visited[s[i]]=True
+                freq[s[i]]-=1
+        return "".join([chr(item) for item in stack])
