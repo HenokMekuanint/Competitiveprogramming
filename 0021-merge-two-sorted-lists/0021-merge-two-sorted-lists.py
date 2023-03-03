90e1eec7743c30baa472be6908085b98a314de01
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-03-03 14:36:13 +0300

    Time: 40 ms (58.87%), Space: 14 MB (23.25%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,37 +5,36 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1 and not list2:
-            return 
-        elif not list1:
-            return list2
-        elif not list2:
-            return list1
-        if list1.val>list2.val:
-            head=ListNode(list2.val)
-            list2=list2.next
-            rtn=head
-        else:
-            head=ListNode(list1.val)
-            rtn=head
-            list1=list1.next
-        while list1 and list2:
-            if list1.val>list2.val:
-                head.next=ListNode(list2.val)
-                head=head.next
-                list2=list2.next
-            else:
-                head.next=ListNode(list1.val)
-                head=head.next
+        dummy=ListNode(-1)
+        def comp_and_build(list1: Optional[ListNode],list2:Optional[ListNode],dummy):
+            if list1 and list2:
+                if list1.val>=list2.val:
+                    dummy.next=ListNode(list2.val)
+                    dummy=dummy.next
+                    list2=list2.next
+                    comp_and_build(list1,list2,dummy)
+                else:
+                    dummy.next=ListNode(list1.val)
+                    dummy=dummy.next
+                    list1=list1.next
+                    comp_and_build(list1,list2,dummy)
+            elif list1:
+                dummy.next=ListNode(list1.val)
+                dummy=dummy.next
                 list1=list1.next
-        while list1:
-            head.next=ListNode(list1.val)
-            head=head.next
-            list1=list1.next
-        while list2:
-            head.next=ListNode(list2.val)
-            head=head.next
-            list2=list2.next
-        return rtn
+                comp_and_build(list1,list2,dummy)
+            elif list2:
+                dummy.next=ListNode(list2.val)
+                dummy=dummy.next
+                list2=list2.next
+                comp_and_build(list1,list2,dummy)
+            elif not list1 and not list2:
+                return None
+        
+                    
+        comp_and_build(list1,list2,dummy)
+        return dummy.next
+                    
+            
             
         
