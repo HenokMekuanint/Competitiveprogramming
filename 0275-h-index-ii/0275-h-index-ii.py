43c7967d32dd92068be7bec5ca12aed9ca41ba1e
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-03-08 23:03:56 +0300

    Time: 148 ms (56.44%), Space: 20.7 MB (21.33%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        left=0
+        right=len(citations)-1
+        while left<=right:
+            mid=(left+right)//2
+            if citations[mid]==len(citations)-mid:
+                return citations[mid]
+            elif citations[mid]>len(citations)-mid:
+                right=mid-1
+            else:
+                left=mid+1
+        return len(citations)-left
