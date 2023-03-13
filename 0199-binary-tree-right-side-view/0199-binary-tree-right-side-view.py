a1a44a3bbc3f7c8e0447ced7a49e205825f85e77
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-03-14 00:27:32 +0300

    Time: 34 ms (66.32%), Space: 14 MB (15.07%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        dicti=defaultdict(list)
+        def traverse(root,level):
+            if root:
+                dicti[level].append(root.val)
+                traverse(root.left,level+1)
+                traverse(root.right,level+1)
+        traverse(root,0)
+        for num in dicti:
+            ans.append(dicti[num][-1])
+        return ans
+                
