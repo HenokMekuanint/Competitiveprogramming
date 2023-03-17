4872bcfc1b46dca1df2cfb26e25d2757df2a1d25
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-03-17 14:50:37 +0300

    Time: 47 ms (64.49%), Space: 20 MB (7.49%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.py
@@ -6,20 +6,23 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        
-        if root == None: return 0
-        result = 1
-        Q = [[root, 0]]
-        while len(Q) > 0:
-            count = len(Q)
-            start = Q[0][1]
-            end = Q[-1][1]
-            result = max(result, end-start+1)
-            for i in range(count):
-                p = Q[0]
-                idx = p[1] - start
-                Q.pop(0)
-                if (p[0].left != None): Q.append([p[0].left, 2*idx+1])
-                if p[0].right != None: Q.append([p[0].right, 2*idx+2])
-            
-        return result
+        min_ans,max_ans=float("inf"),float("-inf")
+        dicti=defaultdict(list)
+        def traverse(root,level,index):
+            if not root:
+                return
+            if root:
+                dicti[level].append(index)
+                traverse(root.left,level+1,2*index)
+                traverse(root.right,level+1,(2*index)+1)
+        traverse(root,0,0)
+        temp=list(dicti.values())
+        for i in range(len(temp)-1,-1,-1):
+            if len(temp[i])>1:
+                if max(temp[i])-min(temp[i])>max_ans-min_ans:
+                    min_ans=min(temp[i])
+                    max_ans=max(temp[i])
+        if max_ans==float("-inf") and min_ans==float("inf"):
+            return 1
+        else:
+            return max_ans-min_ans+1
