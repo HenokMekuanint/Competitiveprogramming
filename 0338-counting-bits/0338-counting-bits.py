f27b013a8f8fb524c35539c8edb660c9314ea994
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-03-29 18:48:47 +0300

    Time: 214 ms (8.73%), Space: 20.8 MB (19.07%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,13 +1,11 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # 0010
-        # 0110
-        # >>
+        
         ans=[0 for i in range(n+1)]
         for num in range(1,n+1):
             index=num
             while num>0:
-                if num%2==1:
+                if num & 1:
                     ans[index]+=1
                 num=num>>1
         return ans
