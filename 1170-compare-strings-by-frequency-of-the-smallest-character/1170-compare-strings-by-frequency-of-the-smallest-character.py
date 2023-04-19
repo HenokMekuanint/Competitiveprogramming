db55e057df81d700ef9b1114c2f216dc3b91df05
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-04-19 13:43:11 +0300

    Time: 105 ms (48.12%), Space: 14.5 MB (86.04%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.py
@@ -7,18 +7,26 @@ class Solution:
             return freq[freq_keys[0]]
         for i in range(len(queries)):
             queries[i]=freq_small(queries[i])
-            
         for j in range(len(words)):
             words[j]=freq_small(words[j])
         words.sort()
         ans=[]
         for query in queries:
-            for j in range(len(words)):
-                if words[j]>query:
-                    ans.append(len(words)-j)
-                    break
-                elif j==len(words)-1 and words[j]<=query:
-                    ans.append(0)
+            left=0
+            right=len(words)-1
+            left_most_index=None
+            while left<=right:
+                mid=(left+right)//2
+                if words[mid]>query:
+                    left_most_index=mid
+                    right=mid-1
+                    
+                else:
+                    left=mid+1
+            if left_most_index==None:
+                ans.append(0)
+            else:ans.append(len(words)-left_most_index)
+                
         return ans
                     
                 
