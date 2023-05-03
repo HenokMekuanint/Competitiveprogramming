e6e3cfd0804fd5f0d95ccaba676d83c98db6fcf4
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-05-03 11:24:07 +0300

    Time: 795 ms (21.18%), Space: 20.1 MB (5.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,27 +1,31 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        visited = set()
-        queue = deque()
-
-        def check(r, c):
-            return 0<=r<len(mat) and 0<=c<len(mat[0]) and (r,c) not in visited
+        directions=[(1,0),(-1,0),(0,1),(0,-1)]
+        def in_bound(row,col):
+            return 0<=row<=len(mat)-1 and 0<=col<=len(mat[0])-1
         
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if mat[i][j] == 0:
-                    visited.add((i, j))
-                    queue.append((i, j))
-                else:
-                    mat[i][j] = -1
-        while queue:
-            row, col = queue.popleft()
-            for d in directions:
-                new_row = row + d[0]
-                new_col = col + d[1]
-                if check(new_row, new_col):
-                    mat[new_row][new_col] = mat[row][col] + 1
-                    queue.append((new_row, new_col))
-                    visited.add((new_row, new_col))
-        return mat
+        ans=[[float("inf") for i in range(len(mat[0]))]for j in range(len(mat))]
+        queue=deque()
+        for r in range(len(mat)):
+            for c in range(len(mat[r])):
+                if mat[r][c]==0:
+                    ans[r][c]=0
+                    queue.append((r,c))
+        def bfs():
+            while queue:
+                row,col=queue.popleft()
+                
+                for change_row,change_col in directions:
+                    new_row=row+change_row
+                    new_col=col+change_col
+                    if in_bound(new_row,new_col) and ans[new_row][new_col]>ans[row][col]+1:
+                        ans[new_row][new_col]=ans[row][col]+1
+                        queue.append((new_row,new_col))
+            return ans
+        return bfs()
+    
+                
+                
+            
+            
         
