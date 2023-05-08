cfb306e7f24bbc149b622018692fcd356d57b0ae
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-05-08 15:19:22 +0300

    Time: 73 ms (14.03%), Space: 16.6 MB (5.62%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,29 +1,14 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        dicti=defaultdict(int)
-        for word in words:
-            dicti[word]+=1
-        dicti=dict(sorted(dicti.items(),key=lambda x:x[1],reverse=True))
-        ans=[]
-        comp=list(dicti.values())[:k]
-        comp=set(comp)
-        for key in dicti:
-            if dicti[key] in comp:
-                ans.append((key,dicti[key]))
-        ans_dicti2=defaultdict(list)
-        for stri,index in ans:
-            ans_dicti2[index].append(stri)
+        freq=Counter(words)
+        list_word=[]
+        for word,frequency in freq.items():
+            heapq.heappush(list_word,(-1*frequency,word))
         
-        for key in ans_dicti2:
-            ans_dicti2[key]=sorted(ans_dicti2[key])
-        rtn=[]
-        for key in ans_dicti2:
-            for word in ans_dicti2[key]:
-                rtn.append(word)
-                if len(rtn)==k:
-                    return rtn
-        return rtn
-        
-            
+        result = []
+        for i in range(k):
+            result.append(heapq.heappop(list_word)[1])
+
+        return result
             
         
