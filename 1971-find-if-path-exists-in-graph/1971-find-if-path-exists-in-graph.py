24656346866714047f3cfcd305fa26b29348ed2b
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-05-17 18:08:17 +0300

    Time: 2030 ms (52.89%), Space: 108.7 MB (53.95%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,19 +1,35 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        dicti=defaultdict(list)
-        for neigh1,neigh2 in edges:
-            dicti[neigh1].append(neigh2)
-            dicti[neigh2].append(neigh1)
-        def dfs(vertex,visited):
-            nonlocal destination
-            if  vertex==destination:
-                return True
-            visited.add(vertex)
-            for neighbour in dicti[vertex]:
-                if neighbour not in visited:
-                    found=dfs(neighbour, visited)
-                    if found:
-                        return found
-    
-            return False
-        return dfs(source,set())
+        parent=defaultdict(int)
+        for i in range(n):
+            parent[i]=i
+        rank=[0 for i in range(n)]
+        def find(x):
+            # compress=[]
+            while x!=parent[x]:
+                # compress.append(x)
+                x=parent[x]
+            # for node in compress:
+            #     parent[node]=x
+            return x
+            
+        def union(x,y):
+            parentX=find(x)
+            parentY=find(y)
+            if parentX==parentY:return
+            
+            if rank[parentY]==rank[parentY]:
+                rank[parentX]+=1
+            if rank[parentX]>rank[parentY]:
+                parent[parentY]=parentX
+            else:
+                parent[parentX]=parentY
+        for frm,to in edges:
+            union(frm,to)
+        return find(source)==find(destination)
+                
+                
+                
+                
+            
+            
