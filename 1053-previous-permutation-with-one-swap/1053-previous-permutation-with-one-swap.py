11265ecb6ad8f1bea6b539118178a09f67a7f111
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-05-30 18:25:53 +0300

    Time: 251 ms (11.69%), Space: 17.8 MB (34.27%) - LeetHub

---
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -0,0 +1,32 @@
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+        flag=True
+        for i in range(1,len(arr)):
+            if arr[i]<arr[i-1]:
+                flag=False
+        if flag:return arr
+        stack=[len(arr)-1]
+        
+        for i in range(len(arr)-2,-1,-1):
+            if stack and arr[stack[-1]]<arr[i]:
+            
+                while stack and arr[stack[-1]]<arr[i]:
+                    popped_index=stack.pop()
+                arr[i],arr[popped_index]=arr[popped_index],arr[i]
+                return arr
+            else:
+                while stack and arr[stack[-1]]==arr[i]:
+                    stack.pop()
+                stack.append(i)
+        return arr
+            
+                
+                
+            
+                    
+        
+            
+            
+            
+                
+        
