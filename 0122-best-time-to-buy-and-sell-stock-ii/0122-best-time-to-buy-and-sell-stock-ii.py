adad30f3a0896c33878af6851d49e837ea2ca3e8
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-06-05 13:49:50 +0300

    Time: 137 ms (5.21%), Space: 49.1 MB (5.01%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        memo={}
+        def dp(index,buy):
+            if index>=len(prices):
+                return 0
+            
+            
+            if (index,buy) in memo:
+                return memo[(index,buy)]
+            
+            if buy:
+                memo[(index,buy)]=max(-prices[index]+dp(index+1,0),0+dp(index+1,1))
+            else:
+                memo[(index,buy)]=max(prices[index]+dp(index+1,1),0+dp(index+1,0))
+            return memo[(index,buy)]
+        return dp(0,1)
