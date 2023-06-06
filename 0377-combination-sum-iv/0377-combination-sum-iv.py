9b29612ff99c8796876933039dacf0f44d53ec6e
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-06-06 17:20:47 +0300

    Time: 57 ms (38.01%), Space: 16.6 MB (8.47%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,26 +1,26 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         nums.sort()
-        memo = {}
+        memo = defaultdict(int)
         
         def dp(cur_sum):
-            if cur_sum == target:
+            if cur_sum == 0:
                 return 1
-            if cur_sum > target:
+            if cur_sum < 0:
                 return 0
             if cur_sum in memo:
                 return memo[cur_sum]
             
             count = 0
-            for i in range(len(nums)):
-                if cur_sum + nums[i] > target:
+            for num in nums:
+                if cur_sum - num < 0:
                     break
-                count += dp(cur_sum + nums[i])
+                count += dp(cur_sum - num)
             
             memo[cur_sum] = count
             return count
         
-        return dp(0)
+        return dp(target)
     
             
              
