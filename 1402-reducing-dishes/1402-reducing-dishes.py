76d9b29fd3868d8bb53a98bdc695254fac0ecdd5
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-07-19 22:06:09 +0300

    Time: 1207 ms (5.02%), Space: 186.8 MB (5.48%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -5,14 +5,10 @@ class Solution:
         def dp(index,count):
             if index==len(satisfaction):
                 return 0
-            
             if memo[(index,count)]:
-                return memo[(index,count)]
-            
+                return memo[(index,count)]     
             memo[(index,count)]=max((satisfaction[index]*count)+dp(index+1,count+1),dp(index+1,count))
             return memo[(index,count)]
-        
-        
         return dp(0,1)
     
         
