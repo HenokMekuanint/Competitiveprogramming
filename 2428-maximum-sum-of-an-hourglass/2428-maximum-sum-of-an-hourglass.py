db5f3d500dafb5dcfeab4a46145120bc2bd2798c
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-09-21 19:05:59 +0300

    Time: 212 ms (96.39%), Space: 19.1 MB (97.47%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -3,18 +3,17 @@ class Solution:
         
         ans=0
         cur_sum=0
-        prev_sum=0
+        
         for row in range(len(grid)-2):
             
             for col in range(len(grid[row])-2):
                 if  col==0:
                     cur_sum=grid[row][col]+grid[row][col+1]+grid[row][col+2]+grid[row+1][col+1]+grid[row+2][col]+grid[row+2][col+1]+grid[row+2][col+2]
-                    prev_sum=cur_sum
+                    
                     ans=max(ans,cur_sum)
                 else:
                     
-                    cur_sum=prev_sum+(grid[row][col+2]-grid[row][col-1]+grid[row+1][col+1]-grid[row+1][col]+grid[row+2][col+2]-grid[row+2][col-1])
-                    prev_sum=cur_sum
+                    cur_sum=cur_sum+(grid[row][col+2]-grid[row][col-1]+grid[row+1][col+1]-grid[row+1][col]+grid[row+2][col+2]-grid[row+2][col-1])
                     ans=max(ans,cur_sum)
 
         return ans
