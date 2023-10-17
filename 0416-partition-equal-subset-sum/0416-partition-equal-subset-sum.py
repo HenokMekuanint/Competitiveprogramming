859da81910feb3d65f86dc3c0e13ce754f5fdf27
Henok Mekuanint <90408697+HenokMekuanint@users.noreply.github.com>
2023-10-17 17:16:45 +0300

    Time: 2254 ms (21.15%), Space: 492.7 MB (5.03%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,20 +1,24 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        n = len(nums)
-        memo = [[-1] * (sum(nums) + 1) for _ in range(n)]
         arrsum=sum(nums)
+        if arrsum%2!=0:
+            return False
+        memo={}
+        
         def dp(index,summission):
-            nonlocal arrsum
+            state=(index,summission)
+            
             if index>=len(nums):
                 if 2*summission==arrsum:
                     return True
                 else:
                     return False
-            if memo[index][summission] != -1:
-                return memo[index][summission]
-
-            memo[index][summission] = dp(index + 1, summission + nums[index]) or dp(index + 1, summission)
-            return memo[index][summission]
+                
+            if state in memo:
+                return memo[state]
+            memo[state]=False
+            memo[state]=dp(index+1,summission+nums[index]) or dp(index+1,summission)
+            return memo[state]
         return dp(0,0)
             
             
